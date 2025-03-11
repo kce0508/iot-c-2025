f6a84bbd0afab2ebbcc897e20ee9344d9fd0283c
kce0508 <kce0508@gmail.com>
2025-03-11 13:53:37 +0900

    커밋

---
+++ Work/C/Day1/name.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("kim Chae Eun");
+	
+	return 0;
+
+};
---
+++ Work/C/Day1/printf3.c
@@ -0,0 +1,11 @@
+///* 문자 */
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("%c\n", 'a');		// 문자는 ''로 감싼다.
+//	printf("홍길동\n");
+//	printf("%s\n", "홍길동");
+//
+//	return 0;
+//}
